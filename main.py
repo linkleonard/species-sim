0bd77f2f3aa40e6f7fc59aa3d3bb6a06e01dde63
Leonard Law <leonwlaw@gmail.com>
2017-12-02 16:51:17 -0500

    Setup overall logic for simulation

--- main.py
+++ main.py
@@ -3,6 +3,11 @@ import os.path
 import yaml
 
 
+def simulate_species_in_habitat(species, habitat, simulation_years):
+    for year in range(simulation_years):
+        pass
+
+
 def main():
     parser = get_argument_parser()
     arguments = parser.parse_args()
@@ -10,11 +15,15 @@ def main():
 
     # TODO: Do we need to validate the configuration file?
     with open(config_path) as config_file:
-        config = yaml.safe_load(config_file)
+        configuration = yaml.safe_load(config_file)
 
-    # TODO: Until we actually run an import, just print out the config so we're
-    # doing *something*.
-    print(config)
+    species_list = configuration['species']
+    habitats = configuration['habitats']
+
+    simulation_years = 100
+    for species in species_list:
+        for habitat in habitats:
+            simulate_species_in_habitat(species, habitat, simulation_years)
 
 
 def get_argument_parser():
