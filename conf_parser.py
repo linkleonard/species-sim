157ddbe6dcbcee725de6d2d0983b06a64f142a8d
Leonard Law <leonwlaw@gmail.com>
2017-12-03 23:56:29 -0500

    Fix monthly food consumption not being loaded

--- conf_parser.py
+++ conf_parser.py
@@ -2,8 +2,11 @@ from models import Habitat, Species
 
 
 def species_from_config(config):
+    attributes = config['attributes']
+
     species = Species()
-    species.life_span = config['attributes']['life_span']
+    species.life_span = attributes['life_span']
+    species.monthly_food_consumption = attributes['monthly_food_consumption']
 
     return species
 
