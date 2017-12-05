189ff3c672df40b5e9bef24e5d7d006794ef34e2
Leonard Law <leonwlaw@gmail.com>
2017-12-04 19:19:06 -0500

    Set levels on logging handlers
    Set stream to WARN
    Set file to DEBUG

--- main.py
+++ main.py
@@ -27,14 +27,19 @@ from conf_parser import habitat_from_config, species_from_config
 male_ratio = 0.5
 
 logger = logging.getLogger()
-logger.setLevel(logging.WARN)
-logger.addHandler(logging.StreamHandler())
+logger.setLevel(logging.DEBUG)
+
+stream_handler = logging.StreamHandler()
+stream_handler.setLevel(logging.WARN)
 
 log_path = os.path.join(
     os.path.dirname(__file__),
     'output.log',
 )
-logger.addHandler(logging.FileHandler(log_path))
+
+file_handler = logging.FileHandler(log_path)
+file_handler.setLevel(logging.DEBUG)
+logger.addHandler(file_handler)
 
 
 def get_initial_simulation_step():
